147ae0fccc112f353c2aeedc31bda530b9b16e3e
wugluskr1 <54072591+wugluskr1@users.noreply.github.com>
2023-08-24 11:55:50 +0300

    modified:   functions.py

--- functions.py
+++ functions.py
@@ -2,5 +2,20 @@ lst=input(("Enter a string:"))
 lst = lst.lower()
 lst = lst.split()
 print(lst)
-def palindrom(lst):
-    pass
+def func(lst):
+    for i in lst:
+        lst[i] = check[::-1]
+        if(lst[i]==check[::-1]): 
+print(lst)
+func()
+
+
+
+
+
+
+
+
+
+
+
