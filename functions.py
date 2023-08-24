50c39976d1be246ac68659d4a7709fe26ee741f9
wugluskr1 <54072591+wugluskr1@users.noreply.github.com>
2023-08-24 14:29:15 +0300

    добавил удаление слов из одного слова

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ lst = input(("Enter a string:"))
 lst = lst.lower()
 lst = lst.split()
 print(lst)
+lst = list(filter(lambda x: len(x) != 1, lst))
+print(lst)
 print(list(filter(lambda x: x == x[::-1], lst)))
 
 
